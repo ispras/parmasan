12253307cbf173c34d972b768680dc2e175cbdbd
Артем Климов <jakmobius@gmail.com>
2024-02-22 10:49:55 +0000

    Remove .hpp from CMakeLists.txt

--- check-format.sh
+++ check-format.sh
@@ -27,6 +27,7 @@ max_line_length=100
 directories=("daemon" "tracer")
 file_extensions=(".cpp" ".hpp" ".c" ".h")
 files=()
+cmakelists=()
 
 for dir in "${directories[@]}"; do
     for ext in "${file_extensions[@]}"; do
@@ -36,6 +37,12 @@ for dir in "${directories[@]}"; do
             fi
         done < <(find "$dir" -name "*$ext" -print0)
     done
+
+    while IFS= read -r -d $'\0' file; do
+        if git ls-files --error-unmatch "$file" &>/dev/null; then
+            cmakelists+=("$file")
+        fi
+    done < <(find "$dir" -name "CMakeLists.txt" -print0)
 done
 
 formatting_errors=0
@@ -53,6 +60,13 @@ for file in "${files[@]}"; do
     # Check if any lines in the file are longer than max_line_length
     if grep -q ".\{$max_line_length\}" "$file"; then
         echo "File $file contains lines longer than $max_line_length characters."
+        formatting_errors=1
+    fi
+done
+
+for cmakelist in "${cmakelists[@]}"; do
+    if grep -Ewq '[[:alnum:]]*\.h(|pp)' "$cmakelist"; then
+        echo "Header file encountered in $cmakelist"
         formatting_errors=1
     fi
 done
